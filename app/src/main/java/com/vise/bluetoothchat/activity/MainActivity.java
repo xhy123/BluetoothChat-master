f8ab71bffd76c65260ccc8cce7e8cdfb03380133
谢华炎 <xhy@szningyi.com>
2018-08-20 14:37:01 +0800

    BluetoothChat

---
+++ app/src/main/java/com/vise/bluetoothchat/activity/MainActivity.java
@@ -0,0 +1,386 @@
+package com.vise.bluetoothchat.activity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import com.vise.basebluetooth.BluetoothChatHelper;
+import com.vise.basebluetooth.callback.IChatCallback;
+import com.vise.basebluetooth.common.State;
+import com.vise.basebluetooth.mode.BaseMessage;
+import com.vise.basebluetooth.utils.BluetoothUtil;
+import com.vise.bluetoothchat.R;
+import com.vise.bluetoothchat.adapter.GroupFriendAdapter;
+import com.vise.bluetoothchat.common.AppConstant;
+import com.vise.bluetoothchat.mode.FriendInfo;
+import com.vise.bluetoothchat.mode.GroupInfo;
+import com.vise.common_base.manager.AppManager;
+import com.vise.common_base.utils.ToastUtil;
+import com.vise.common_utils.log.LogUtils;
+import com.vise.common_utils.utils.character.DateTime;
+import com.vise.common_utils.utils.view.ActivityUtil;
+
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends BaseChatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ExpandableListView mGroupFriendLv;
+    private GroupFriendAdapter mGroupFriendAdapter;
+    private List<GroupInfo> mGroupFriendListData = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initWidget() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // ActivityUtil.startForwardActivity(MainActivity.this, AddFriendActivity.class);
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mGroupFriendLv = (ExpandableListView) findViewById(R.id.friend_group_list);
+        Log.d("chatbleussss","wifiAPAdmin.getMacAddress()sssss =    "+getMobileMAC(this)) ;
+    }
+
+    @Override
+    protected void initData() {
+        mGroupFriendAdapter = new GroupFriendAdapter(mContext, mGroupFriendListData);
+        mGroupFriendLv.setAdapter(mGroupFriendAdapter);
+        mGroupFriendLv.expandGroup(0);
+
+        if(BluetoothUtil.isSupportBle(mContext)){
+            BluetoothUtil.enableBluetooth((Activity) mContext, 1);
+
+        } else{
+            ToastUtil.showToast(mContext, getString(R.string.phone_not_support_bluetooth));
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    AppManager.getAppManager().appExit(mContext);
+                }
+            }, 3000);
+        }
+    }
+
+    public static String getMac(Context context)
+    {
+        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        return info.getMacAddress();
+    }
+
+    /**
+     * 兼容Android 6.0以上设备获取MAC地址
+     * 物理地址、硬件地址，用来定义网络设备的位置
+     * 兼容原因：从android 6.0之后，android 移除了通过 WiFi 和蓝牙 API 来在应用程序中可编程的访问本地硬件标示符。现在 WifiInfo.getMacAddress() 和 BluetoothAdapter.getAddress() 方法都将返回 02:00:00:00:00:00
+     * add by heliquan at 2017年6月12日
+     *
+     * @return
+     */
+    public static String getMobileMAC(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // 如果当前设备系统大于等于6.0 使用下面的方法
+            ReadPhoneStatePermissions((Activity) context);
+            return getHeightMac();
+        } else {
+            return getLowerMac(context);
+        }
+    }
+
+    private static final int READ_PHONE_STATE = 2;
+    private static String[] PERMISSIONS_READ_PHONE = {
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    public static void ReadPhoneStatePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity,
+                Manifest.permission.READ_PHONE_STATE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_READ_PHONE,
+                    READ_PHONE_STATE);
+        }
+    }
+
+    /**
+     * 兼容Android 6.0以下设备获取MAC地址 Android API < 23
+     *
+     * @return
+     */
+    private static String getLowerMac(Context context) {
+        try {
+            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            // 获取MAC地址
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            String mac = wifiInfo.getMacAddress();
+            if (null == mac) {
+                // 未获取到
+                mac = "";
+            }
+            return mac;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * 获取高版本手机的MAC地址 Android API >= 23
+     *
+     * @return
+     */
+    private static String getHeightMac() {
+        String str = "";
+        String macSerial = "";
+        try {
+
+            Process pp = Runtime.getRuntime().exec(
+                    "cat /sys/class/net/wlan0/address ");
+            InputStreamReader ir = new InputStreamReader(pp.getInputStream());
+            LineNumberReader input = new LineNumberReader(ir);
+            for (; null != str; ) {
+                str = input.readLine();
+                if (str != null) {
+                    macSerial = str.trim();// 去空格
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (macSerial == null || "".equals(macSerial)) {
+            try {
+                return loadFileAsString("/sys/class/net/eth0/address")
+                        .toUpperCase().substring(0, 17);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return macSerial;
+    }
+
+    private static String loadFileAsString(String fileName) throws Exception {
+        FileReader reader = new FileReader(fileName);
+        String text = loadReaderAsString(reader);
+        reader.close();
+        return text;
+    }
+
+    private static String loadReaderAsString(Reader reader) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        char[] buffer = new char[4096];
+        int readLength = reader.read(buffer);
+        while (readLength >= 0) {
+            builder.append(buffer, 0, readLength);
+            readLength = reader.read(buffer);
+        }
+        return builder.toString();
+    }
+
+    @Override
+    protected void initEvent() {
+        mGroupFriendLv.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                FriendInfo friendInfo = mGroupFriendListData.get(groupPosition).getFriendList().get(childPosition);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(AppConstant.FRIEND_INFO, friendInfo);
+                ActivityUtil.startForwardActivity(MainActivity.this, ChatActivity.class, bundle, false);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                findDevice();
+            } else{
+                AppManager.getAppManager().appExit(mContext);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onRestart() {
+        if(BluetoothUtil.isSupportBle(mContext)){
+            BluetoothUtil.enableBluetooth((Activity) mContext, 1);
+        } else{
+            ToastUtil.showToast(mContext, getString(R.string.phone_not_support_bluetooth));
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    AppManager.getAppManager().appExit(mContext);
+                }
+            }, 3000);
+        }
+        super.onRestart();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+       // getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_about) {
+            displayAboutDialog();
+            return true;
+        } else if(id == R.id.menu_share){
+            ToastUtil.showToast(mContext, getString(R.string.menu_share));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void displayAboutDialog() {
+        final int paddingSizeDp = 5;
+        final float scale = getResources().getDisplayMetrics().density;
+        final int dpAsPixels = (int) (paddingSizeDp * scale + 0.5f);
+
+        final TextView textView = new TextView(this);
+        final SpannableString text = new SpannableString(getString(R.string.about_dialog_text));
+
+        textView.setText(text);
+        textView.setAutoLinkMask(RESULT_OK);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setPadding(dpAsPixels, dpAsPixels, dpAsPixels, dpAsPixels);
+
+        Linkify.addLinks(text, Linkify.ALL);
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.menu_about)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, null)
+                .setView(textView)
+                .show();
+    }
+
+    private void findDevice(){
+        // 获得已经保存的配对设备
+        Set<BluetoothDevice> pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            mGroupFriendListData.clear();
+            GroupInfo groupInfo = new GroupInfo();
+            groupInfo.setGroupName(BluetoothAdapter.getDefaultAdapter().getName());
+            List<FriendInfo> friendInfoList = new ArrayList<>();
+            for (BluetoothDevice device : pairedDevices) {
+                FriendInfo friendInfo = new FriendInfo();
+                friendInfo.setIdentificationName(device.getName());
+                friendInfo.setDeviceAddress(device.getAddress());
+                friendInfo.setFriendNickName(device.getName());
+                friendInfo.setOnline(false);
+                friendInfo.setJoinTime(DateTime.getStringByFormat(new Date(), DateTime.DEFYMDHMS));
+                friendInfo.setBluetoothDevice(device);
+                friendInfoList.add(friendInfo);
+            }
+            groupInfo.setFriendList(friendInfoList);
+            groupInfo.setOnlineNumber(0);
+            mGroupFriendListData.add(groupInfo);
+            mGroupFriendAdapter.setGroupInfoList(mGroupFriendListData);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/vise/bluetoothchat/newutils/BLECommunicateUtils.java
@@ -0,0 +1,103 @@
+package com.vise.bluetoothchat.newutils;
+
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+/**
+ * Created by zrf on 2016/4/27.
+ */
+public class BLECommunicateUtils {
+
+
+    public static BluetoothLeService mBLEService = null;
+
+    /**
+     * After new a bleService ,must call the method
+     *
+     * @param mBLEService mBLEService
+     */
+    public static void setBLEService(final BluetoothLeService mBLEService) {
+        BLECommunicateUtils.mBLEService = mBLEService;
+    }
+
+    public static BluetoothLeService getBLEService() {
+        return mBLEService;
+    }
+
+    /**
+     * receive data / read data
+     *
+     * @param gattCharacteristic gattCharacteristic
+     */
+    public static void receiveData(BluetoothGattCharacteristic gattCharacteristic) {
+        if (mBLEService != null && gattCharacteristic != null) {
+            mBLEService.readCharacteristic(gattCharacteristic);
+        }
+    }
+
+    /**
+     * send data/ write data
+     *
+     * @param gattCharacteristic gattCharacteristic
+     * @param bytes              bytes
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void sendData(BluetoothGattCharacteristic gattCharacteristic, byte[] bytes) {
+        gattCharacteristic.setValue(bytes);
+        gattCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+        mBLEService.writeCharacteristic(gattCharacteristic);
+    }
+
+
+    /**
+     * send data
+     *
+     * @param gattCharacteristic gattCharacteristic
+     * @param str                str
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void sendData(BluetoothGattCharacteristic gattCharacteristic, String str) {
+        if (mBLEService != null && gattCharacteristic != null) {
+            gattCharacteristic.setValue(str);
+            mBLEService.writeCharacteristic(gattCharacteristic);
+        }
+    }
+
+    /**
+     * when read or write data ,will trigger the listener
+     *
+     * @param onDataAvailableListener onDataAvailableListener
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void setOnCharacteristicDataListener(BluetoothLeService.OnDataAvailableListener onDataAvailableListener) {
+        if (mBLEService != null) {
+            mBLEService.setOnDataAvailableListener(onDataAvailableListener);
+        }
+    }
+
+    /**
+     * when connect a remote device /the state is connected ,will trigger the listener
+     *
+     * @param onConnectListener onConnectListener
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void setOnConnectListener(BluetoothLeService.OnConnectListener onConnectListener) {
+        if (mBLEService != null) {
+            mBLEService.setOnConnectListener(onConnectListener);
+        }
+    }
+
+    /**
+     * when disconnect a remote device /the state is disconnected ,will trigger the listener
+     *
+     * @param onDisconnectListener OnDisconnectListener
+     */
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public static void setOnDisConnectListener(BluetoothLeService.OnDisconnectListener onDisconnectListener) {
+        if (mBLEService != null) {
+            mBLEService.setOnDisconnectListener(onDisconnectListener);
+        }
+    }
+}
